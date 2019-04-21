8774f3e3d52ef47b5853a495475c20a5d9376cb6
Higor Ferreira <hfashigor@hotmail.com>
2019-04-21 11:40:08 -0300

    Working for 5, 6, 7 and 8 characters

--- main.cpp
+++ main.cpp
@@ -7,37 +7,72 @@ using namespace std;
 
 #define RANGE_START 0x20
 #define RANGE_ENDDD 0X7E
-#define DISPERCION "ab"
+#define DISPERTION "ab"
 
 //Declaração do protótipo da função
 // char *crypt(const char *, const char *);
 
-string pass5(int start, int end, string &compare){
+string pass5(int start8, int start7, int start6, int start, string &compare){
 
-    string kicker = "";
-    string cryptKick = "";
+    string shot = "";
+    string shot6 = "";
+    string shot7 = "";
+    string shot8 = "";
+    
+    string cryptShot = "";
+    string cryptShot6 = "";
+    string cryptShot7 = "";
+    string cryptShot8 = "";
 
-    for(int i = start; i < end; i++){
-        for(int j = RANGE_START; j < RANGE_ENDDD; j++){
-            for(int k = RANGE_START; k < RANGE_ENDDD; k++){
-                for(int l = RANGE_START; l < RANGE_ENDDD; l++){
-                    for(int m = RANGE_START; m < RANGE_ENDDD; m++){
-                        kicker += i;
-                        kicker += j;
-                        kicker += k;
-                        kicker += l;
-                        kicker += m;
+    for(int c = start8; c < RANGE_ENDDD; c++){
+        for(int b = start7; b < RANGE_ENDDD; b++){
+            for(int a = start6; a < RANGE_ENDDD; a++){
 
-                        cout<<kicker<<endl;
+                for(int i = start; i < RANGE_ENDDD; i++){
+                    for(int j = RANGE_START; j < RANGE_ENDDD; j++){
+                        for(int k = RANGE_START; k < RANGE_ENDDD; k++){
+                            for(int l = RANGE_START; l < RANGE_ENDDD; l++){
+                                for(int m = RANGE_START; m < RANGE_ENDDD; m++){
+                                    //Shot 5 characters
+                                    shot += i;
+                                    shot += j;
+                                    shot += k;
+                                    shot += l;
+                                    shot += m;
 
-                        cryptKick = crypt(kicker.c_str(), DISPERCION);
-                        
-                        if(cryptKick == compare) return kicker;
+                                    //Shot 6 characters
+                                    shot6 += a;
+                                    shot6 += shot;
 
-                        // Cleanning kicker
-                        kicker = "";
+                                    //Shot 7 characters
+                                    shot7 += b;
+                                    shot7 += shot6;
+
+                                    //Shot 8 characters
+                                    shot8 += c;
+                                    shot8 += shot7;
+
+                                    cryptShot = crypt(shot.c_str(), DISPERTION);
+                                    cryptShot6 = crypt(shot6.c_str(), DISPERTION);
+                                    cryptShot7 = crypt(shot7.c_str(), DISPERTION);
+                                    cryptShot8 = crypt(shot8.c_str(), DISPERTION);
+                                    
+                                    if(cryptShot == compare) return shot;
+                                    if(cryptShot6 == compare) return shot6;
+                                    if(cryptShot7 == compare) return shot7;
+                                    if(cryptShot8 == compare) return shot8;
+
+                                    // Cleanning shots
+                                    shot = "";
+                                    shot6 = "";
+                                    shot7 = "";
+                                    shot8 = "";
+                                }
+                            }
+                        }
                     }
                 }
+
             }
         }
     }
@@ -45,12 +80,12 @@ string pass5(int start, int end, string &compare){
 
 //ABSTRAÇÃO DE CÓDIGO PARA O PROCESSAMENTO EM PARALELO
 int main(){
-    string pass = crypt("  ana", DISPERCION);
+    string pass = crypt("     ana", DISPERTION);
     // string pass = "ANA";
     
     int size,rank;
 
-    string adv = pass5(0x20, 0x21, pass);
+    string adv = pass5(0x20, 0x20, 0x20, 0x20, pass);
 
     cout<<endl;
     cout<<"Senha: "<<adv<<endl;
